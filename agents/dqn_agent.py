b2eb6e4ccea838165abc41aab973822b63a4d467
Piero Macaluso <pieromacaluso8@gmail.com>
2021-04-02 15:13:24 +0200

    Improving DQN Script

--- agents/dqn_agent.py
+++ agents/dqn_agent.py
@@ -14,13 +14,11 @@ class DQNAgent(Agent):
                  buffer_size=int(1e5), batch_size=64, gamma=0.99, tau=1e-3, lr=5e-4, update_every=4, double_qn=True):
         super().__init__(state_size, action_size, seed, device, buffer_size, batch_size, gamma, tau, lr, update_every)
         # Q-Network
-        self.qnetwork_local = QNet(state_size, action_size, seed).to(device)
-        self.qnetwork_target: QNet = QNet(state_size, action_size, seed).to(device)
-        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=self.lr)
-
+        self.q_online = QNet(state_size, action_size, seed).to(device)
+        self.q_target: QNet = QNet(state_size, action_size, seed).to(device)
+        self.optimizer = optim.Adam(self.q_online.parameters(), lr=self.lr)
         # Replay memory
         self.memory = ReplayBuffer(self.action_size, self.buffer_size, self.batch_size, self.seed, self.device)
-
         self.double_qn = double_qn
 
     def step(self, state, action, reward, next_state, done):
@@ -50,10 +48,10 @@ class DQNAgent(Agent):
 
         """
         state = torch.from_numpy(state).float().unsqueeze(0).to(self.device)
-        self.qnetwork_local.eval()
+        self.q_online.eval()
         with torch.no_grad():
-            action_values = self.qnetwork_local(state)
-        self.qnetwork_local.train()
+            action_values = self.q_online(state)
+        self.q_online.train()
 
         # Epsilon-greedy action selection
         if random.random() > eps:
@@ -73,22 +71,25 @@ class DQNAgent(Agent):
         """
         states, actions, rewards, next_states, dones = experiences
         if self.double_qn:
-            best_action_q = self.qnetwork_local(next_states).detach().max(1)[1]
-            targets_next = self.qnetwork_target(next_states).detach()[
+            best_action_q = self.q_online(next_states).detach().max(1)[1]
+            targets_next = self.q_target(next_states).detach()[
                 np.arange(self.batch_size), best_action_q].unsqueeze(1)
         else:
-            targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
+            targets_next = self.q_target(next_states).detach().max(1)[0].unsqueeze(1)
         targets = rewards + (gamma * targets_next * (1 - dones))
-        expected = self.qnetwork_local(states).gather(1, actions)
+        expected = self.q_online(states).gather(1, actions)
         # Compute loss
         loss = F.mse_loss(expected, targets)
         # Minimize the loss
         self.optimizer.zero_grad()
         loss.backward()
+        # Gradient Clipping
+        for param in self.q_online.parameters():
+            param.grad.data.clamp_(-1, 1)
         self.optimizer.step()
 
         # ------------------- update target network ------------------- #
-        DQNAgent.soft_update(self.qnetwork_local, self.qnetwork_target, self.tau)
+        DQNAgent.soft_update(self.q_online, self.q_target, self.tau)
 
     @staticmethod
     def soft_update(local_model, target_model, tau):
@@ -103,3 +104,7 @@ class DQNAgent(Agent):
         """
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
             target_param.data.copy_(tau * local_param.data + (1.0 - tau) * target_param.data)
+
+    def load_weights(self, pth_path):
+        self.q_online.load_state_dict(torch.load(pth_path))
+        self.q_target.load_state_dict(torch.load(pth_path))
